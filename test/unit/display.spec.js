3a292b046e2a885ee1c1a9d78150a7c6e54c38e1
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-07-02 00:32:26 +0200

    Added some tests regarding #hide()

--- test/unit/display.spec.js
+++ test/unit/display.spec.js
@@ -4,6 +4,9 @@ describe('unit/show.spec.js', function() {
 
 	beforeEach(function() {
 		dp = new ctor();
+
+		// We don't care about rendering. We will just make this a noop
+		dp.render = function() { return document.createElement('div'); };
 	});
 
 	describe('When calling show()', function() {
@@ -12,14 +15,10 @@ describe('unit/show.spec.js', function() {
 
 		describe('with valid parameters', function() {
 			beforeEach(function() {
-				var elm = document.createElement('div')
-
-				// We don't care about rendering. We will just make this a noop
-				dp.render = function() { return document.createElement('div'); };
-
 				showSpy = sinon.spy();
 				dp.on('show', showSpy);
 
+				var elm = document.createElement('div')
 				showResult = dp.show(elm);
 			});
 
@@ -53,6 +52,38 @@ describe('unit/show.spec.js', function() {
 				expect(function() {
 					dp.show('invalid');
 				}).to['throw']('"invalid" does not resolve to an element!');
+			});
+		});
+	});
+
+	describe('When calling "hide()"', function() {
+		describe('before calling show', function() {
+			it('should not throw', function() {
+				expect(function hide() {
+					dp.hide();
+				}).not.to.throw();
+			});
+			it('should not emit hide', function() {
+				var spy = sinon.spy()
+				dp.on('hide', spy);
+				dp.hide();
+				expect(spy).not.to.have.been.called;
+			});
+		});
+		describe('after calling show', function() {
+			var hideSpy
+			beforeEach(function() {
+				var elm = document.createElement('div')
+
+				hideSpy = sinon.spy();
+				dp.on('hide', hideSpy);
+
+				dp.show(elm);
+				dp.hide();
+			});
+
+			it('should emit a hide-event', function() {
+				expect(hideSpy).to.have.been.called;
 			});
 		});
 	});
